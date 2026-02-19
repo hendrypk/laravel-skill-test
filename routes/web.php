078ef0e3591270f68998a84dfe7e80fe3e9ac34d
Hendry Putra Kurniawan <hendryputra934@gmail.com>
2026-02-19 09:28:08 +0700

    change: route web

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,7 @@ Route::middleware('auth')->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
-    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::match(['put', 'patch'], 'posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::resource('posts', PostController::class);
 });
 
 require __DIR__.'/settings.php';
